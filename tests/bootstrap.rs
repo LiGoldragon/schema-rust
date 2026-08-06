664335240a40728826cfaa09e3100cd867031912
li <li@goldragon.criome.net>
2026-08-06 17:07:19 +0200

    schema-rust: unify strict bootstrap producer train (primary-hqu.11.3)

--- tests/bootstrap.rs
+++ tests/bootstrap.rs
@@ -607,9 +607,11 @@ fn nonempty_interface_roles_are_exact_nomos_refusals() {
 #[test]
 fn strict_bootstrap_lane_pins_one_exact_verified_producer_train() {
     for exact_dependency in [
-        "core-ethos = { git = \"https://github.com/LiGoldragon/core-ethos.git\", rev = \"427fd74a8217f557cfabdfe0ff23b784c855bf85\" }",
-        "core-nomos = { git = \"https://github.com/LiGoldragon/core-nomos.git\", rev = \"ff675b264e3650afb88291036ec2bdd97292a2e7\" }",
-        "sema-translator = { git = \"https://github.com/LiGoldragon/sema-translator.git\", rev = \"6b5499c0d25c801b56582fdcd8e021c3293a6d4d\", default-features = false, features = [\"bootstrap\"] }",
+        "core-ethos = { git = \"https://github.com/LiGoldragon/core-ethos.git\", rev = \"43b48c779c54ee9f05cbcc111d5d88074b162461\" }",
+        "core-nomos = { git = \"https://github.com/LiGoldragon/core-nomos.git\", rev = \"7b60721d199551b648d42a49934a2f0ef950c595\" }",
+        "rust-logos = { git = \"https://github.com/LiGoldragon/rust-logos.git\", rev = \"081e99596826b15e2ff7f1356ae8d797b18aeffc\" }",
+        "sema-translator = { git = \"https://github.com/LiGoldragon/sema-translator.git\", rev = \"287fbd728a05b1a6be1dc8a28bcf3ca06d9916b3\", default-features = false, features = [\"bootstrap\"] }",
+        "signal-sema-translator = { git = \"https://github.com/LiGoldragon/signal-sema-translator.git\", rev = \"3f41813dd63904c7e2b3da4382eff64ed1bf12fe\" }",
     ] {
         assert!(
             MANIFEST.contains(exact_dependency),
